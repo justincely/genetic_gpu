d614289e54b3282760217946f252494b2f7949ab
Justin Ely <justincely@gmail.com>
2018-11-21 21:32:54 -0500

    faster!

--- assignment.cu
+++ assignment.cu
@@ -21,38 +21,8 @@ using namespace std;
 
 const double MAX = 5.12;
 const double MIN = -5.12;
-const int n_population = 100;
-const int n_parameters = 10;
-
-/**
-    Convolves an array with a window, using shared memory for caching.
-
-    @param n the size of the array.
-    @param *a the pointer to the array being convolved.
-    @param size the size of the convolution window.
-    @param iterations the number of convolutions to go through.
-    @param *w the pointer to the convolution window.
- */
-__global__ void generateIndividuals(double a[n_population][n_parameters], double min, double max) {
-
-        int index = blockIdx.x * blockDim.x + threadIdx.x;
-        int stride = blockDim.x * gridDim.x;
-
-        printf("Hello from block %d, thread %d. Current total index %d, using stride size %d\n", blockIdx.x, threadIdx.x, index, stride);
-        /**
-           for (int i=index; i<n_population; i+=stride) {
-           for (int p=0; p<n_parameters; p++) {
-
-            double myrandf = curand_uniform(randState+i);
-            //myrandf *= (max_rand_int[index] - min_rand_int[index]+0.999999);
-            //myrandf += min_rand_int[index];
-            printf("%f\n", myrandf);
-            a[i] = myrandf;
-            //a[i] = static_cast <double> (rand()) / (static_cast <double> (RAND_MAX/max));
-           }
-           }
-         */
-}
+const int n_population = 1000;
+const int n_parameters = 100;
 
 
 struct prg
@@ -150,28 +120,49 @@ int min_index(thrust::device_vector<T>& vec) {
 }
 
 
-__host__ thrust::device_vector<double> evaluateGeneration(thrust::device_vector<double> population){
-        thrust::device_vector<double> popScores(n_population);
+__global__ void score(unsigned int n, unsigned int np, double *source, double *score) {
+        int index = blockIdx.x * blockDim.x + threadIdx.x;
+        int stride = blockDim.x * gridDim.x;
 
+        double value;
+
+        if (index < n) {
+                for (int i=index; i < n; i += stride) {
+                        value = 0;
+                        for (int p=0; p<np; p++) {
+                                value += source[i*np+p];
+                        }
+
+                        value = (double) std::sqrt( (double) value);
+                        score[i] = (double) 1.0 / (double) (value+1.0);
+                }
+        }
+}
+
+template <class T>
+void evaluateGeneration(thrust::device_vector<T>& popScores, thrust::device_vector<T>& population) {
         thrust::device_vector<double> temp(population.size());
         thrust::copy(thrust::device, population.begin(), population.end(), temp.begin());
 
         thrust::transform(temp.begin(), temp.end(), temp.begin(), minus5<double>());
         thrust::transform(temp.begin(), temp.end(), temp.begin(), square<double>());
 
-        for (int i=0; i<n_population; i++) {
-                // std::cout << "Individual " << i << ":  ";
-                // for (int offset=0; offset<n_parameters; offset++) {
-                //         std::cout << population[i*n_parameters+offset] << " ";
-                // }
+        double* tempPtr = thrust::raw_pointer_cast(&temp[0]);
+        double* scoresPtr = thrust::raw_pointer_cast(&popScores[0]);
 
-                double score = std::sqrt(thrust::reduce(temp.begin()+i*n_parameters, temp.begin()+i*n_parameters+n_parameters, (double) 0, thrust::plus<double>()));
-                popScores[i] = (double) 1.0 / (double) (score+1.0);
+        score<<<512, 256>>>(n_population, n_parameters, tempPtr, scoresPtr);
 
-                // std::cout << " - Score: " << popScores[i] << endl;
-        }
-
-        return popScores;
+        // for (int i=0; i<n_population; i++) {
+        //         // std::cout << "Individual " << i << ":  ";
+        //         // for (int offset=0; offset<n_parameters; offset++) {
+        //         //         std::cout << population[i*n_parameters+offset] << " ";
+        //         // }
+        //
+        //         double score = std::sqrt(thrust::reduce(temp.begin()+i*n_parameters, temp.begin()+i*n_parameters+n_parameters, (double) 0, thrust::plus<double>()));
+        //         popScores[i] = (double) 1.0 / (double) (score+1.0);
+        //
+        //         // std::cout << " - Score: " << popScores[i] << endl;
+        // }
 }
 
 __host__ thrust::device_vector<double> breed(thrust::device_vector<double> parentA, thrust::device_vector<double> parentB, int crossover){
@@ -222,7 +213,8 @@ int main(int argc, char** argv) {
 
         int generation = 0;
 
-        thrust::device_vector<double> popScores = evaluateGeneration(population);
+        thrust::device_vector<double> popScores(n_population);
+        evaluateGeneration(popScores, population);
         double best = *(thrust::max_element(popScores.begin(), popScores.end()));
         int best_index = min_index(popScores);
 
@@ -232,9 +224,26 @@ int main(int argc, char** argv) {
         }
         std::cout << endl;
 
+        // begin timing functions
+        float elapsedTime;
+        cudaEvent_t startEvent, stopEvent;
+        // Create start and stop events
+        cudaEventCreate(&startEvent);
+        cudaEventCreate(&stopEvent);
 
-        while (generation < 100) {
+        cudaEvent_t startEvent2, stopEvent2;
+        // Create start and stop events
+        cudaEventCreate(&startEvent2);
+        cudaEventCreate(&stopEvent2);
+
+        while (best < .99) {
+                // Time array creation
+                cudaEventRecord(startEvent, 0);
+
                 thrust::device_vector<double> newPopulation(n_population * n_parameters);
+
+                // Time array creation
+                cudaEventRecord(startEvent2, 0);
 
                 int parentsPool[n_population];
                 for (int n=0; n<n_population; n+=2) {
@@ -272,6 +281,22 @@ int main(int argc, char** argv) {
                         }
                 }
 
+                // Create stop events
+                cudaEventRecord(stopEvent2, 0);
+                cudaEventSynchronize(stopEvent2);
+
+                // Print total elapsted seconds
+                cudaEventElapsedTime(&elapsedTime, startEvent2, stopEvent2);
+                //std::cout << "Picking parents took " << elapsedTime/1000 << " (seconds)" << endl;
+
+                cudaEventRecord(startEvent2, 0);
+
+                // parent arrays
+                thrust::device_vector<double> parentA(n_parameters);
+                thrust::device_vector<double> parentB(n_parameters);
+                // children
+                thrust::device_vector<double> childA(n_parameters);
+                thrust::device_vector<double> childB(n_parameters);
                 for (int n=0; n<n_population; n+=2) {
                         //cout << "Parent A: " << parentsPool[n] << " Score: " << popScores[parentsPool[n]];
                         //cout <<" Parent B: " << parentsPool[n+1] << " Score: " << popScores[parentsPool[n]];
@@ -287,14 +312,14 @@ int main(int argc, char** argv) {
                         } else {
                                 int crossover = rand()%n_parameters;
 
-                                thrust::device_vector<double> parentA(n_parameters);
+                                //thrust::device_vector<double> parentA(n_parameters);
                                 thrust::copy(thrust::device, population.begin()+indexA*n_parameters, population.begin()+indexA*n_parameters+n_parameters, parentA.begin());
 
-                                thrust::device_vector<double> parentB(n_parameters);
+                                //thrust::device_vector<double> parentB(n_parameters);
                                 thrust::copy(thrust::device, population.begin()+indexB*n_parameters, population.begin()+indexB*n_parameters+n_parameters, parentB.begin());
 
-                                thrust::device_vector<double> childA = breed(parentA, parentB, crossover);
-                                thrust::device_vector<double> childB = breed(parentB, parentA, crossover);
+                                childA = breed(parentA, parentB, crossover);
+                                childB = breed(parentB, parentA, crossover);
 
                                 if (rand()%100 < 5) {
                                         int randIndex = rand()%n_parameters;
@@ -326,20 +351,61 @@ int main(int argc, char** argv) {
 
 
                 }
+                // Create stop events
+                cudaEventRecord(stopEvent2, 0);
+                cudaEventSynchronize(stopEvent2);
+
+                // Print total elapsted seconds
+                cudaEventElapsedTime(&elapsedTime, startEvent2, stopEvent2);
+                //std::cout << "Creating children took " << elapsedTime/1000 << " (seconds)" << endl;
 
 
+                // Time array creation
+                cudaEventRecord(startEvent2, 0);
 
                 thrust::copy(thrust::device, newPopulation.begin(), newPopulation.end(), population.begin());
-                popScores = evaluateGeneration(population);
+
+                // Create stop events
+                cudaEventRecord(stopEvent2, 0);
+                cudaEventSynchronize(stopEvent2);
+
+                // Print total elapsted seconds
+                cudaEventElapsedTime(&elapsedTime, startEvent2, stopEvent2);
+                //std::cout << "First copy " << elapsedTime/1000 << " (seconds)" << endl;
+
+                cudaEventRecord(startEvent2, 0);
+                evaluateGeneration(popScores, population);
+                // Print total elapsted seconds
+                // Create stop events
+                cudaEventRecord(stopEvent2, 0);
+                cudaEventSynchronize(stopEvent2);
+                cudaEventElapsedTime(&elapsedTime, startEvent2, stopEvent2);
+                //std::cout << "compute scores " << elapsedTime/1000 << " (seconds)" << endl;
 
                 best = *(thrust::min_element(popScores.begin(), popScores.end()));
                 best_index = min_index(popScores);
 
+                // Create stop events
+                cudaEventRecord(stopEvent2, 0);
+                cudaEventSynchronize(stopEvent2);
+
+                // Print total elapsted seconds
+                cudaEventElapsedTime(&elapsedTime, startEvent2, stopEvent2);
+                //std::cout << "Copy to new gen took " << elapsedTime/1000 << " (seconds)" << endl;
+
                 std::cout << "Bred generation " << generation << " Best score: " << best << " at index: " << best_index << "   ";
-                for (int i=0; i<n_parameters; i++) {
-                        std::cout << population[best_index * n_parameters + i] << " ";
-                }
+                //for (int i=0; i<n_parameters; i++) {
+                //        std::cout << population[best_index * n_parameters + i] << " ";
+                //}
                 std::cout << endl;
+
+                // Create stop events
+                cudaEventRecord(stopEvent, 0);
+                cudaEventSynchronize(stopEvent);
+
+                // Print total elapsted seconds
+                cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
+                //std::cout << "Generation took " << elapsedTime/1000 << " (seconds)" << endl;
 
                 generation++;
         }
